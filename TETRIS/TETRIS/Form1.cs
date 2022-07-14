b1a4b3e476cfdc5a30eb8d736cff5ab3d8ce7341
Juha-Matti Mäyränpää <juha-matti.mayranpaa@codea.fi>
2022-07-14 15:07:18 +0300

    Step2 done

---
+++ TETRIS/TETRIS/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TETRIS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadCanvas();
+        }
+        Bitmap canvasBitmap;
+        Graphics canvasGraphics;
+        int canvasWidth = 15;
+        int canvasHeight = 20;
+        int[,] canvasDotArray;
+        int dotSize = 20;
+
+        private void loadCanvas()
+        {
+            // Rezise the picture box based on the dotsize and canvas size
+            pictureBox1.Width = canvasWidth * dotSize;
+            pictureBox1.Height = canvasHeight * dotSize;
+
+            //Create Bitmap with picture box's size
+            canvasBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            canvasGraphics = Graphics.FromImage(canvasBitmap);
+
+            canvasGraphics.FillRectangle(Brushes.LightGray, 0, 0, canvasBitmap.Width, canvasBitmap.Height);
+
+            // Load bitmap into picture box
+            pictureBox1.Image = canvasBitmap;
+
+            // Initialize canvas dot array. By default all elements are zero
+            canvasDotArray = new int[canvasWidth, canvasHeight];
+
+
+        }
+
+    }
+}
